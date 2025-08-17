dcf767aa1496c4e50b6e75b68ca55e49f4c96ede
Suraj Kumar Saw <suraj03saw@gmail.com>
2025-08-17 15:44:20 +0530

    Create home_screen.dart

---
+++ src/screens/home_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("Home"),
+            centerTitle: true,
+          ),
+      body: Center(
+        child: Text("HomeScreen"),
+      ),
+    ));
+  }
+}
