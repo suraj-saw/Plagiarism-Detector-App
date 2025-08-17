3776bbe07c7be654d9140d628971cb506540f117
Suraj Kumar Saw <suraj03saw@gmail.com>
2025-08-17 15:45:07 +0530

    Create onBoarding_controller.dart

---
+++ src/screens/onBoardingScreen/onBoarding_controller.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class OnBoardingController extends GetxController {
+  PageController indicator = PageController();
+  RxInt page = 0.obs;
+}
