73fe667aa43b31d014e29a9e4159b8b2a0f9b2d2
Suraj Kumar Saw <suraj03saw@gmail.com>
2025-08-22 16:03:52 +0530

    Update elevated_button_theme.dart

--- src/themes/elevated_button_theme.dart
+++ src/themes/elevated_button_theme.dart
@@ -4,22 +4,22 @@ class TElevatedButtonTheme {
   TElevatedButtonTheme._();
 
   static final lightElevatedButtonTheme = ElevatedButtonThemeData(
-      style: ElevatedButton.styleFrom(
+    style: ElevatedButton.styleFrom(
     elevation: 0,
-    foregroundColor: Colors.black,
-    backgroundColor: Colors.grey.shade500,
-    side: const BorderSide(color: Colors.grey),
-    padding: const EdgeInsets.symmetric(vertical: 5),
-    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+    // foregroundColor: Colors.white,
+    backgroundColor: Colors.green,
+    //side: const BorderSide(color: Colors.white),
+    // padding: const EdgeInsets.symmetric(vertical: 5),
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
   ));
 
   static final darkElevatedButtonTheme = ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
     elevation: 0,
-    foregroundColor: Colors.white,
-    backgroundColor: Colors.purple,
-    side: const BorderSide(color: Colors.white),
-    padding: const EdgeInsets.symmetric(vertical: 5),
-    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+    // foregroundColor: Colors.black,
+    backgroundColor: Colors.lightGreen,
+    //side: const BorderSide(color: Colors.white),
+    // padding: const EdgeInsets.symmetric(vertical: 5),
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
   ));
 }
