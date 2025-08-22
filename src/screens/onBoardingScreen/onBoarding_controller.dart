34807b897f6edbba8d11bc79502e2c0afbe20593
Suraj Kumar Saw <suraj03saw@gmail.com>
2025-08-22 16:06:39 +0530

    Delete src/screens directory

--- src/screens/onBoardingScreen/onBoarding_controller.dart
+++
@@ -1,9 +0,0 @@
-// ignore_for_file: file_names
-
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-
-class OnBoardingController extends GetxController {
-  PageController indicator = PageController();
-  RxInt page = 0.obs;
-}
